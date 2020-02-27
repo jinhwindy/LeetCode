e0ba2cbe412bec24f18afd7f63dd76f11acab1da
jinhwindy <34299534+jinhwindy@users.noreply.github.com>
2020-02-27 17:15:38 +0800

    https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/
    答案来自评论区
    
    虽然本题只是普通二叉树，但是依然满足如下的几条特性：
    1：在遍历的过程中，先出现的节点同时也是公共祖先
    2：如果本节点不是p或q，则遍历该节点的左右子树：若左右子树中分别出现了p、q，则该节点时祖先
    若其中一个为空，则p、q位于同一侧，则p、q有一个已经出现，则返回的节点就是祖先

---
+++ lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,22 @@
+public class Solution {//所有的递归的返回值有4种可能性，null、p、q、公共祖先
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {//当遍历到叶结点后就会返回null
+            return root;
+        }
+        //如果先出现一个，那么公共祖先就一定是它了
+        if (root == p || root == q) {//当找到p或者q的是时候就会返回pq
+            return root;/*当然，值得一提的是，如果公共祖先是自己（pq），并不需要寻找另外
+                     一个，我们在执行前序遍历会先找上面的，后找下面的，我们会直接返回公共祖先。*/
+        }
+        TreeNode left = LowestCommonAncestor(root.left, p, q);//返回的结点进行保存，可能是null
+        TreeNode right = LowestCommonAncestor(root.right, p, q);//也可能是pq，还可能是公共祖先
+        if (left != null && right != null) {
+            return root;//如果左右都存在，就说明pq都出现了，这就是，公共祖先，此时不用考虑公共祖先是自己的情况，因为上面已经做过判断了。
+        } else if (left != null) {//否则我们返回已经找到的那个值（存储在left，与right中），p或者q
+            return left;//还有一种可能就是，由下面返回的公共祖先，并将这个值一路返回到最表层
+        } else if (right != null) {
+            return right;
+        }
+        return null;
+    }
+}
